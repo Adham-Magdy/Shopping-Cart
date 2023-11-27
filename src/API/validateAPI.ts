37891ab632b8c73e00367e0ff02c7767257974d0
Adham-Magdy <adhammagdy349@gmail.com>
2023-11-27 13:04:38 +0200

    added comment on validation method

--- src/API/validateAPI.ts
+++ src/API/validateAPI.ts
@@ -1,5 +1,6 @@
 import { Product } from "../products/productsSlice";
 
+// validate title and price that user entered
 const validation = (product: Product) => new Promise<Product>((resolve,reject)=> setTimeout(()=>{
     if(product.tittle.length === 0){
         reject("NO Title");
